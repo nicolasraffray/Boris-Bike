3f7ca8479d8a3ee3ff92df02aec8a71eaf817f45
nicolasraffray <nraffray@gmail.com>
2019-12-10 18:14:11 +0000

    Q10

--- spec/boris_bikes_spec.rb
+++ spec/boris_bikes_spec.rb
@@ -2,4 +2,9 @@ require "DockingStation"
 
 describe DockingStation	 do
 	it { is_expected().to respond_to :release_bike }
+
+	it "Get me the bike and I want it to work pls" do
+		bike = subject.release_bike
+		expect(bike.working?).to eq true
+	end
 end
